b675f7a01ca6bb0050d461d894ff70d038496f49
JaylynDawson <jaylyn.dawson@gmail.com>
2014-06-27 15:07:03 -0700

    getting the MVC to work

--- template.php
+++ template.php
@@ -7,16 +7,16 @@
     <script src="main.js"></script>
 </head>
 <body>
-    
+
     <?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php"; ?>
 
-    <div class="main">
-        <div class="contentContainer"
+    <main>
+        <div class="contentContainer">
             <h1>I've:</h1>
             <a href="#" class="button">Lost Something</a>
             <a href="#" class="button">Found Something</a>
         </div>
-    </div>
+    <main>
 
     <?php include ROOT . "/footer.php"; ?>
 
